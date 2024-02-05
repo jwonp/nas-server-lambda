24996b8eb6d4ef300ba25d48cd54995e563d9c46
Joowon <tkdel222@gmail.com>
2024-02-05 16:02:28 +0900

    add meta data type

---
+++ src/types/MetaData.ts
@@ -0,0 +1,23 @@
+/**
+ * ex.
+ *  @directory  /example1/example2
+ *  @ownerId    session.user.username
+ */
+export type MetaData = {
+  directory: string;
+  fileName: string;
+  ownerId: string;
+  key: string;
+  uploadTime: number;
+  type: FileType;
+  size: number;
+};
+
+export const fileTypes = {
+  file: "file",
+  folder: "folder",
+  image: "image",
+  video: "video",
+} as const;
+
+export type FileType = (typeof fileTypes)[keyof typeof fileTypes];
