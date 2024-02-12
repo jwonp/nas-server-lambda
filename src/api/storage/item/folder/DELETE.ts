ef4df891af1d4b378bca6748d25f9df7804506bc
Joowon <tkdel222@gmail.com>
2024-02-12 22:08:41 +0900

    add API  DELETE /storage/item/folder

---
+++ src/api/storage/item/folder/DELETE.ts
@@ -0,0 +1,96 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import admin from "firebase-admin";
+import * as serviceAccount from "../../../../../firebase-admin-key.json";
+
+import { DeleteObjectsCommand, S3Client } from "@aws-sdk/client-s3";
+
+import { FIREBASE_COLLECTION } from "../../../../libs/firebase/collections";
+import { VolumeSize } from "../../../../entity/Volume";
+import { FieldValue } from "firebase-admin/firestore";
+
+const firebaseAdmin = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+const db = admin.firestore();
+const client = new S3Client({});
+exports.handler = async (event: APIGatewayProxyEvent) => {
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
+  if (!event.body) {
+    response.statusCode = 400;
+    return response;
+  }
+  const { fileId, fileSize, userId, directory } = JSON.parse(event.body);
+  const userVolumeDocs = await db
+    .collection(FIREBASE_COLLECTION.USERS)
+    .doc(userId)
+    .collection(FIREBASE_COLLECTION.VOLUME)
+    .get();
+  if (userVolumeDocs.size !== 1) {
+    response.statusCode = 400;
+    response.body = JSON.stringify({ error: "Error on loading user volume" });
+    return response;
+  }
+
+  if (!fileId) {
+    response.statusCode = 400;
+    return response;
+  }
+  console.log(fileId);
+  const targetFilest = await db
+    .collection(FIREBASE_COLLECTION.USERS)
+    .doc(userId)
+    .collection(FIREBASE_COLLECTION.STORAGES).where("directory");
+  const command = new DeleteObjectsCommand({
+    Bucket: process.env.BUCKET_NAME as string,
+    Delete: {
+      Objects: [
+        {
+          Key: fileId,
+        },
+      ],
+    },
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+
+  const deleteDocs = await db
+    .collection(FIREBASE_COLLECTION.USERS)
+    .doc(userId)
+    .collection(FIREBASE_COLLECTION.STORAGES)
+    .where("key", "==", fileId)
+    .get();
+  if (deleteDocs.size !== 1) {
+    response.statusCode = 400;
+    response.body = JSON.stringify({ error: "Target file is not unique" });
+    return response;
+  }
+
+  const target = deleteDocs.docs.map((doc) => doc.id)[0];
+  console.log(target);
+  db.collection(FIREBASE_COLLECTION.USERS)
+    .doc(userId)
+    .collection(FIREBASE_COLLECTION.STORAGES)
+    .doc(target)
+    .delete();
+
+  const userVolumeDocId = userVolumeDocs.docs.map((doc) => doc.id)[0];
+  const userVolume = userVolumeDocs.docs.map((doc) =>
+    doc.data()
+  )[0] as VolumeSize;
+
+  db.collection(FIREBASE_COLLECTION.USERS)
+    .doc(userId)
+    .collection(FIREBASE_COLLECTION.VOLUME)
+    .doc(userVolumeDocId)
+    .update({ now: FieldValue.increment(-1 * fileSize) });
+
+  return response;
+};
